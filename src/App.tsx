a91b61c5b3b2486d97ad3cc8c7b3885a0467f41e
daeho15 <daeho15.kim@gmail.com>
2024-10-26 23:11:18 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,65 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import mappings from './mappings.json'
+import './fixed-button.css'; // 
 
 function App() {
-    const [searchTerm, setSearchTerm] = useState('1');
-    const handleNextSearch = () => {
-        setSearchTerm((parseInt(searchTerm)+1).toString())
-        const encodedSearchTerm = encodeURIComponent('scs-02 examtopics ' + searchTerm);
-        const luckyUrl = `https://www.google.com/search?q=${encodedSearchTerm}&btnI`;
-        window.open(luckyUrl, '_blank');
-    };
+    const [index, setIndex] = useState(window.localStorage.getItem('index') || '1')
+    const [clicked, setClicked] = useState(false)
+
+    useEffect(() => {
+        window.localStorage.setItem('index', (parseInt(index)).toString())
+    }, [index])
+
+
+    const handleClick = () => {
+        setClicked(true)
+        const map = mappings[parseInt(index) - 1]
+        const nextIndex = (parseInt(index) + 1).toString()
+        if (map) {
+            const url = map.link
+            setIndex(nextIndex)
+            // setNext(nextIndex)
+            window.location.href = url
+        } else {
+            setIndex(nextIndex)
+            // setNext(nextIndex)
+            const encodedSearchTerm = encodeURIComponent('scs-02 examtopics ' + index);
+            const luckyUrl = `https://www.google.com/search?q=${encodedSearchTerm}&btnI`;
+            window.location.href = luckyUrl
+        }
+    }
+
+    
     return (
         <>
             <div>
-                <h3>지금 : SCS-C02 examtopics {searchTerm}</h3>
+                <h3>SCS-C02 examtopics</h3>
             </div>
+            <hr />
             <div>
                 <input
-                    type="text"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
+                    type="number"
+                    value={index}
+                    onChange={(e) => setIndex(e.target.value)}
                     placeholder="검색어를 입력하세요"
+                    onKeyUp={(e) => {
+                        if (e.key === 'Enter') {
+                            handleClick();
+                        }
+                    }}
                 />
-                <button onClick={handleNextSearch}>GOGO</button>
+                <button
+                    id='button'
+                    onClick={handleClick}
+                >Go</button>
             </div>
+            <button
+                className="fixed-button"
+                onClick={handleClick}
+            >
+               Go
+            </button>
         </>
     );
 }
